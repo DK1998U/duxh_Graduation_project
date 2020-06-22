91ed3476c8f013d1de0f4f8731353727e249d928
DK1998U <50914781+DK1998U@users.noreply.github.com>
2020-06-22 10:59:46 +0800

    Add files via upload

---
+++ d_edge_crf_s.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+print(tf.__version__)
+print("d_edge_crf_update_5_24")
+import numpy as np
+import math
+import cv2
+#以下代码仅为测试所用，核心代码稍后公开
+
+
+def inside_get_loss(c):
+    t1 = 0
+    return t1
+def get_ecrf_conv1(kernel_size, wavelength, angle, shift, deviation, ratio):
+    t1 = 0
+    return t1
+
+def ecrf_loss_gabor_sadd(label, pre):
+    t1 = 0
+    return t1
+
+
+def get_sobel():
+    t1 = 0
+    return t1
+def d_sobel_loss_sadd(label, pre):
+    t1 = 0
+    return t1
+
+
+def d_mse_loss(label, pre):
+    return tf.math.reduce_mean(tf.math.square(label - pre))
+
+def gaussian_kernel_2d_opencv(kernel_size = 3,sigma = 0):
+    t1 = 0
+    return t1
+
+def ecrf_loss_gaussian_s(label, pre):
+    t1 = 0
+    return t1
+
+
+
+
+
+
+
